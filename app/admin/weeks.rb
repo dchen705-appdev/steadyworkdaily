1b11380f956eaf363afa5b1692deac3d6522a9c1
Raghu Betina <raghu@firstdraft.com>
2018-03-14 23:36:11 +0000

    Whitelist columns in Week for ActiveAdmin

--- app/admin/weeks.rb
+++ app/admin/weeks.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Week do
+
+ permit_params :start_datetime, :end_datetime, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
